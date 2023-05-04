78b057c9cb4484e108c8e6230c49800a3c042e88
Xoldarov Temur <69597351+xaldarof@users.noreply.github.com>
2023-05-04 22:55:18 +0500

    fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:encrypt_shared_preferences/enc_shared_pref.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:jokes_app/common/resource/colors.dart';
+import 'package:jokes_app/common/resource/fonts.dart';
+import 'package:jokes_app/common/utils/printer.dart';
 import 'package:jokes_app/di/app_di.dart';
 import 'package:jokes_app/ui/main/main_screen.dart';
 import 'package:modified_localization/easy_localization.dart';
-
+import 'package:overlay_support/overlay_support.dart';
 import 'common/language/language.dart';
 
 void main() async {
@@ -11,6 +16,43 @@ void main() async {
   await EasyLocalization.ensureInitialized();
   await EncryptedSharedPreferences.initialize('1234930934-023940-2394-0324234');
   await setUpDependencies();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  late final FirebaseMessaging messaging;
+  messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    provisional: false,
+    sound: true,
+  );
+
+  final res = await FirebaseMessaging.instance.getToken();
+  printMessage("Token : $res");
+
+  FirebaseMessaging.onMessage.listen(
+    (RemoteMessage message) {
+      printMessage(message.toString());
+      showSimpleNotification(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                message.notification?.title ?? "",
+                style: primaryTextStyle(color: AppColors.white),
+              ),
+              Text(
+                message.notification?.body ?? "",
+                style: primaryTextStyle(color: AppColors.white),
+              ),
+            ],
+          ),
+          background: AppColors.darkGreen);
+    },
+  );
+
   runApp(
     EasyLocalization(
       supportedLocales: Language.supportedLanguages,
@@ -20,4 +62,8 @@ void main() async {
       child: const MainScreen(),
     ),
   );
+}
+
+Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  printMessage("Handling a background message: ${message.messageId}");
 }
--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -4,6 +4,7 @@ import 'package:jokes_app/di/app_di.dart';
 import 'package:jokes_app/ui/main/bloc/main_bloc.dart';
 import 'package:jokes_app/ui/main/splash_screen.dart';
 import 'package:modified_localization/easy_localization.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 import '../../common/utils/ui.dart';
 
@@ -20,16 +21,18 @@ class MainScreen extends StatelessWidget {
         ..add(
           ObserveAppState(),
         ),
-      child: MaterialApp(
-        navigatorKey: navigatorKey,
-        locale: context.locale,
-        supportedLocales: context.supportedLocales,
-        localizationsDelegates: context.localizationDelegates,
-        debugShowCheckedModeBanner: false,
-        home: BlocBuilder<MainBloc, MainState>(
-          builder: (BuildContext context, state) {
-            return const SplashScreen();
-          },
+      child: OverlaySupport.global(
+        child: MaterialApp(
+          navigatorKey: navigatorKey,
+          locale: context.locale,
+          supportedLocales: context.supportedLocales,
+          localizationsDelegates: context.localizationDelegates,
+          debugShowCheckedModeBanner: false,
+          home: BlocBuilder<MainBloc, MainState>(
+            builder: (BuildContext context, state) {
+              return const SplashScreen();
+            },
+          ),
         ),
       ),
     );
